3da282f363fcaac0fec42bdf0a422efee2e3fc90
mikyGitlab <hailmichael.aderfs@procure.az>
2023-01-02 21:48:55 +0300

    WIP: feature/FormArray implimentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
 import { Component } from '@angular/core';
-
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { map } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'kfsystem';
+  // Icons
+  faMinus = faMinus;
+  faPlus = faPlus;
+
+  constructor(private fb: FormBuilder) {}
+
+  // FormGroups
+  groupForm: FormGroup = this.fb.group({
+    groups: this.fb.array([]),
+  });
+
+  slotForm: FormGroup = this.fb.group({
+    slots: this.fb.array([]),
+  });
+
+  // Value Getters
+  getter(type: string) {
+    return this.groupForm.controls[type] as FormArray;
+  }
+
+  // Form Group getters
+  getGroupsFG() {
+    return (this.groupForm.controls['groups'] as FormArray)
+      .controls as FormGroup[];
+  }
+
+  getSlotsFG(groupIdx: number) {
+    let slotFormArray = (this.slotForm.controls['slots'] as FormArray)
+      .controls as FormGroup[];
+
+    let filteredFormArray = slotFormArray.filter(
+      (formGroup) => formGroup.value.groupIdx === groupIdx
+    );
+
+    return filteredFormArray;
+  }
+
+  // Events
+  addGroup(): void {
+    const groupForm = this.fb.group({
+      groupName: ['', Validators.required],
+      slots: [[]],
+    });
+    this.getter('groups').push(groupForm);
+    this.slotForm.value.slots = [];
+  }
+
+  addSlot(groupIdx: number): void {
+    const slotForm = this.fb.group({
+      slotName: ['', Validators.required],
+      groupIdx: groupIdx,
+    });
+    this.getter('slots').push(slotForm);
+    this.getter('groups').value[groupIdx].slots = this.getter('slots').value;
+  }
+
+  deleteGroup(groupIdx: number): void {
+    this.getter('groups').removeAt(groupIdx);
+  }
+
+  deleteSlot(groupIdx: number, slotIdx: number): void {
+    this.getter('slots').removeAt(slotIdx);
+    this.getter('groups').value[groupIdx].slots = this.getter('slots').value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgbModule, FontAwesomeModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
